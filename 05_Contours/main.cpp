e4631bcc3a77f7a54436c984abf64210469d2e34
bobbybee <j104@rocketmail.com>
2014-12-27 17:28:30 -0500

    Many updates. Algorithm performs _much_ better now

--- 05_Contours/main.cpp
+++ 05_Contours/main.cpp
@@ -4,13 +4,13 @@
 
 using namespace cv;
 
-static inline double labDelta(Vec3b ref, uint8_t* row, int index) {
+static inline int labDelta(Vec3b ref, uint8_t* row, int index) {
 	Vec3b oldPixel = row[index];
 
 	// check if the colour changed more than a threshold
-	double delta0 = ref[0] - oldPixel[0];
-	double delta1 = ref[1] - oldPixel[1];
-	double delta2 = ref[2] - oldPixel[2];
+	int delta0 = ref[0] - oldPixel[0];
+	int delta1 = ref[1] - oldPixel[1];
+	int delta2 = ref[2] - oldPixel[2];
 	
 	return (delta0 * delta0) + (delta1 * delta1) + (delta2 * delta2);
 }
@@ -19,46 +19,72 @@ int main(int argc, char** argv) {
 	VideoCapture stream(0);
 
 	Mat lastFrame;
+	Mat twoFrame;
+
 	stream.read(lastFrame); // fixes a race condition
+	stream.read(twoFrame);
 
 	for(;;) {
+				printf("Frame\n");
+
 		Mat frame;
 		stream.read(frame);
 		
-		Mat delta = frame.clone();
-		cvtColor(delta, delta, CV_BGR2Lab);
+		//Mat delta = frame.clone();
+		//cvtColor(delta, delta, CV_BGR2Lab);
 
-		int i = delta.cols, j = delta.rows;
+		/*int i = delta.cols, j = delta.rows;
 		uint8_t* row;
+		uint8_t* oldrow;
 
 		while(j--) {
 			row = delta.ptr<uint8_t>(j);
+			oldrow = lastFrame.ptr<uint8_t>(j);
 
 			while(i--) {
 				Vec3b ref = row[i];
-				double totalDelta = labDelta(ref, row, i - 1) + 
-									labDelta(ref, row, i + 1) + 
-									labDelta(ref, row, i);
+				int mine = labDelta(ref, oldrow, i - 1) + labDelta(ref, oldrow, i + 1);
+				//int neighborlDelta = abs(abs(labDelta(ref, oldrow, i - 1) - mine) -
+				//					abs(labDelta(ref, oldrow, i + 1) - mine));
+									
+				//bool hasChanged = neighborlDelta < 25000 && mine > 5000;
+				bool highlight = !(mine > 4000);
 
-				bool hasChanged = totalDelta > 20000;
-
-				row[ (i * 3) ] = hasChanged * 255;
-				row[ (i * 3) + 1] = hasChanged * 255;
-				row[ (i * 3) + 2] = hasChanged * 255;
+				row[ (i * 3) ] = highlight * 255;
+				row[ (i * 3) + 1] = highlight * 255;
+				row[ (i * 3) + 2] = highlight * 255;
 
 			}
 
 			i = delta.cols;
-		}
+		}*/
 
-		for(int i = 1; i < 25; i = i + 2) {
-			blur(delta, delta, Size(i, i), Point(-1, -1));
-		}
+		Mat out1, out2, delta;
 
-		threshold(delta, delta, 50, 255, THRESH_BINARY);
+		absdiff(twoFrame, frame, out1);
+		absdiff(lastFrame, frame, out2);
+		bitwise_and(out1, out2, delta);
 
-		Canny(delta, delta, 20, 20 * 3, 3);
+		//imshow("Delta", delta);
+
+		//imshow("Blurred", delta);
+
+		threshold(delta, delta, 60, 255, THRESH_BINARY);
+		blur(delta, delta, Size(21, 21), Point(-1, -1));
+
+		//imshow("threshold", delta);
+
+		cvtColor(delta, delta, CV_BGR2GRAY);
+
+		//adaptiveThreshold(delta, delta, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 31, 0);
+
+		//imshow("Not canny", delta);
+
+		Canny(delta, delta, 40, 40 * 3, 3);
 		
+		//imshow("Canny", delta);
+
+
 		// obtain contours
 		std::vector<std::vector<Point> > contours;
 		std::vector<Vec4i> hierarchy;
@@ -77,27 +103,19 @@ int main(int argc, char** argv) {
 		for(int i = 0; i < contours.size(); ++i) {
 			approxPolyDP(contours[i], approxShape, arcLength(Mat(contours[i]), true) * 0.04, true);
 			
-			double len = arcLength(Mat(contours[i]), true);
-
-			if(len > maxArclength) {
-				maxArclength = len;
-				contourNumForMax = i;
-			}
+			drawContours(contourDrawing, contours, i, normalColour, 3);
 		}
 
-		for (int i = 0; i < contours.size(); ++i) {
-			if(i == contourNumForMax)
-				drawContours(contourDrawing, contours, i, userColour, 3);	
-			else
-				drawContours(contourDrawing, contours, i, normalColour, 3);
-		}
+		Mat flipped;
+		flip(contourDrawing, flipped, 1);
 
-		imshow("Contours", contourDrawing);
+		imshow("Contours", flipped);
 		
 		if(waitKey(10) == 27) {
 			break;
 		
 		}
+		twoFrame = lastFrame;
 		lastFrame = frame;
 	}
 
