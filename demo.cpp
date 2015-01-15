1e0d3188158984aa4d336b61e8df2f6fdab6de62
bobbybee <j104@rocketmail.com>
2015-01-15 13:36:02 -0500

    Comment and simplify the demo

--- demo.cpp
+++ demo.cpp
@@ -4,8 +4,13 @@
 #include <CommodityTracking.h>
 
 int main(int argc, char** argv) {
+	// initialize camera stream from the built-in webcam
+	// and initialize FrameHistory with that stream
+
 	VideoCapture stream(0);
 	FrameHistory history(stream);
+
+	// automatically calibrate userSensitivity
 
 	int minimumArclength = 150;
 	int userSensitivity = 255;
@@ -13,20 +18,19 @@ int main(int argc, char** argv) {
 
 	autoCalibrateSensitivity(&userSensitivity, stream, history, minimumArclength, 1, limbGracePeriod);
 
-	int showOriginal = 0, showSkeleton = 0, _flip = 0;
+	// settings GUI
+
+	int showOriginal = 0;
 
 	namedWindow("Settings", 1);
 	createTrackbar("Minimum Arc Length", "Settings", &minimumArclength, 500);
 	createTrackbar("Sensitivity", "Settings", &userSensitivity, 1000);
-
 	createTrackbar("Show original?", "Settings", &showOriginal, 1);
-	createTrackbar("Show skeleton?", "Settings", &showSkeleton, 1);
-	createTrackbar("Flip?", "Settings", &_flip, 1);
 
-	Skeleton lastSkeleton;
+	Skeleton lastSkeleton; // last skeleton required for performing tracking
 
 	for(;;) {
-		Mat visualization;
+		Mat visualization; // initialize a backdrop for the skeleton
 
 		if(showOriginal) {
 			visualization = history.getLastFrame().clone();
@@ -35,16 +39,21 @@ int main(int argc, char** argv) {
 		}
 		
 
-		lastSkeleton.visualize(visualization);
+		lastSkeleton.visualize(visualization); // use Skeleton::visualize for the demo
+												// in a real app, we would access properties such as
+												// skeleton.leftMostAbove (left hand position)
 		imshow("Visualization", visualization);
 
 		if(waitKey(1) == 27) {
 			break;
 		}
 
-		Skeleton skeleton = getSkeleton(stream, history, lastSkeleton, _flip, minimumArclength, userSensitivity, limbGracePeriod);
+		// get the Skeleton object
+		// takes VideoStream, FrameHistory, Skeleton lastSkeleton,
+		// whether to flip the frame, and some sensitivity settings
+
+		Skeleton skeleton = getSkeleton(stream, history, lastSkeleton, false, minimumArclength, userSensitivity, limbGracePeriod);
 		lastSkeleton = skeleton;
 	}
-
 	return 0;
 }
