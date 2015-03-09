06c3417fb86641b95511b3faafdc64e6f10dc994
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-09 14:25:02 -0400

    Limb classification to prevent multiple limbs of the same type

--- demo.cpp
+++ demo.cpp
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include <CommodityTracking.h>
 
+Point averagePoints(std::vector<Point> points) {
+	if(points.size()) {
+		int sumX = 0, sumY = 0;
+
+		for(int i = 0; i < points.size(); ++i) {
+			sumX += points[i].x;
+			sumY += points[i].y;
+		}
+
+		return Point(sumX / points.size(), sumY / points.size());
+	} else {
+		return Point(0, 0);
+	}
+}
+
 int main(int argc, char** argv) {
 	// initialize camera stream from the built-in webcam
 	// and initialize FrameHistory with that stream
@@ -57,34 +72,44 @@ int main(int argc, char** argv) {
 
 		// each center is a different skeleton / blob
 		for(int skeleton = 0; skeleton < centers.size(); ++skeleton) {
+			// we may have duplicates even now
+			// remember which so we can assemble a skeleton
+			vector<Point> leftHands, rightHands, leftLegs, rightLegs, unclassifieds;
+
 			// draw limbs
-			for(int limb = 0; limb < edgePointsList[skeleton].size(); ++limb) {
-				//line(visualization, centers[skeleton] * 10, edgePointsList[skeleton][limb] * 10, Scalar(0, 255, 0), 10);
-			
+			for(int limb = 0; limb < edgePointsList[skeleton].size(); ++limb) {			
 				if( (edgePointsList[skeleton][limb].y - centers[skeleton].y) > 20) {
-					rectangle(visualization, edgePointsList[skeleton][limb] * 10, edgePointsList[skeleton][limb] * 10, Scalar(0, 255, 0), 50);
-				
-					int leg = 0; // 0 for left, 1 for right
-
 					if(edgePointsList[skeleton][limb].x - centers[skeleton].x > 0) {
-						leg = 1;
+						rightHands.push_back(edgePointsList[skeleton][limb]);
+					} else {
+						leftHands.push_back(edgePointsList[skeleton][limb]);
 					}
-
-					putText(visualization, leg == 0 ? "L" : "R", edgePointsList[skeleton][limb] * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255));
 				} else if( abs(edgePointsList[skeleton][limb].x - centers[skeleton].x) > 20 ) {
-					rectangle(visualization, edgePointsList[skeleton][limb] * 10, edgePointsList[skeleton][limb] * 10, Scalar(255, 0, 0), 50);
-					
-					int hand = 0; // 0 for left, 1 for right
-
 					if(edgePointsList[skeleton][limb].x - centers[skeleton].x > 0) {
-						hand = 1;
+						rightLegs.push_back(edgePointsList[skeleton][limb]);
+					} else {
+						leftLegs.push_back(edgePointsList[skeleton][limb]);
 					}
-
-					putText(visualization, hand == 0 ? "L" : "R", edgePointsList[skeleton][limb] * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255));
 				} else {
+					unclassifieds.push_back(edgePointsList[skeleton][limb]);
 					rectangle(visualization, edgePointsList[skeleton][limb] * 10, edgePointsList[skeleton][limb] * 10, Scalar(0, 0, 255), 50);
 				}
 			}
+
+			Point rightHand = averagePoints(rightHands), leftHand = averagePoints(leftHands),
+				  rightLeg = averagePoints(rightLegs), leftLeg = averagePoints(leftLegs);
+
+			rectangle(visualization, leftHand * 10, leftHand * 10, Scalar(0, 0, 255), 50);
+			putText(visualization, "L", leftHand * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255));
+
+			rectangle(visualization, rightHand * 10, rightHand * 10, Scalar(0, 0, 255), 50);
+			putText(visualization, "R", rightHand * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255));
+
+			rectangle(visualization, leftLeg * 10, leftLeg * 10, Scalar(0, 255, 0), 50);
+			putText(visualization, "L", leftLeg * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0));
+
+			rectangle(visualization, rightLeg * 10, rightLeg * 10, Scalar(0, 255, 0), 50);
+			putText(visualization, "R", rightLeg * 10, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0));
 
 			// draw the tracking dot
 			rectangle(visualization, centers[skeleton] * 10, centers[skeleton] * 10, Scalar(255, 255, 0), 50);
