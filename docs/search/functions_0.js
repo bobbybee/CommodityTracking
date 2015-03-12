79121c30c505158278fe70c857828fd6c6bb52f6
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-12 13:10:11 -0400

    Update documentation

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['append',['append',['../classct_1_1_frame_history.html#ae02d47a0fb6e72897510e6d9c9c30a2e',1,'ct::FrameHistory']]],
-  ['autocalibratesensitivity',['autoCalibrateSensitivity',['../namespacect.html#a2de4e2c14840541014a051d42b8aca57',1,'ct']]]
+  ['autocalibratesensitivity',['autoCalibrateSensitivity',['../namespacect.html#adc9afa041c4b84691258be719e7d9cd4',1,'ct']]]
 ];
