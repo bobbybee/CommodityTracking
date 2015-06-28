2556d9d8c99268b854931b34d96742ab05475a56
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-06-28 11:57:58 -0400

    Work on fixing doxygen

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['getedgepoints',['getEdgePoints',['../namespacect.html#a5775e19f3d899f271bf4ba3732d0a528',1,'ct']]],
   ['getlastframe',['getLastFrame',['../classct_1_1_frame_history.html#aa8ebea9b8ca60c4a981cfc047c5ce8d0',1,'ct::FrameHistory']]],
-  ['getskeleton',['getSkeleton',['../namespacect.html#a7ef32e29885cbc7132cebf33c5f0edc2',1,'ct']]]
+  ['getskeleton',['getSkeleton',['../namespacect.html#abb8756e0d83d9f7bc2713ba079338324',1,'ct']]]
 ];
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['head',['head',['../classct_1_1_skeleton.html#abf12c8c86c367e7df03ab533abd2af0f',1,'ct::Skeleton']]]
+  ['head',['head',['../classct_1_1_skeleton.html#abf12c8c86c367e7df03ab533abd2af0f',1,'ct::Skeleton']]],
+  ['highusermask',['highUserMask',['../namespacect.html#ac93704c6d49e76e2cc3604636c0ac05e',1,'ct']]]
 ];
